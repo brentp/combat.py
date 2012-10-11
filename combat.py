a733db22ea82a7409557e53846fdcc566bba1bdf
Brent Pedersen <bpederse@gmail.com>
2012-10-11 11:33:03 -0600

    make sure that factors remain in same order

--- combat.py
+++ combat.py
@@ -11,8 +11,12 @@ def adjust_nums(numCovs, drop_idxs):
     if numCovs is None: return drop_idxs
     return [nc - sum(nc < di for di in drop_idxs) for nc in numCovs]
 
-def design_mat(mod, numCovs):
-    design = patsy.dmatrix("~ 0 + C(batch)", mod, return_type="dataframe")
+def design_mat(mod, numCovs, batch_levels):
+    # require levels to make sure they are in the same order as we use in the
+    # rest of the script.
+    design = patsy.dmatrix("~ 0 + C(batch, levels=%s)" % str(batch_levels),
+                                                  mod, return_type="dataframe")
+
     mod = mod.drop(["batch"], axis=1)
     print >>sys.stderr, "found %i batches" % design.shape[1]
     other_cols = [c for i, c in enumerate(mod.columns) if not i in numCovs]
@@ -32,9 +36,11 @@ def combat(dat, batch, mod, numCovs=None):
     if not isinstance(numCovs, (list, tuple)):
         numCovs = [numCovs]
     
-    mod["batch"] = batch
+    mod["batch"] = list(batch)
 
-    batch_info = [v for k, v in mod.groupby("batch").groups.items()]
+    batch_items = mod.groupby("batch").groups.items()
+    batch_levels = [k for k, v in batch_items]
+    batch_info = [v for k, v in batch_items]
     n_batch = len(batch_info)
     n_batches = np.array([len(v) for v in batch_info])
     n_array = float(sum(n_batches))
@@ -45,11 +51,11 @@ def combat(dat, batch, mod, numCovs=None):
     mod = mod[[c for c in mod.columns if not c in drop_cols]]
     numCovs = [list(mod.columns).index(c) if isinstance(c, basestring) else c for c in numCovs]
 
-    design = design_mat(mod, numCovs)
+    design = design_mat(mod, numCovs, batch_levels)
 
+    print >>sys.stderr, "Standardizing Data across genes."
     B_hat = np.dot(np.dot(la.inv(np.dot(design.T, design)), design.T), dat.T)
     grand_mean = np.dot((n_batches / n_array).T, B_hat[:n_batch,:])
-
     var_pooled = np.dot(((dat - np.dot(design, B_hat).T)**2), np.ones((n_array, 1)) / n_array)
 
     stand_mean = np.dot(grand_mean.T.reshape((len(grand_mean), 1)), np.ones((1, n_array)))
@@ -59,6 +65,7 @@ def combat(dat, batch, mod, numCovs=None):
 
     s_data = ((dat - stand_mean) / np.dot(np.sqrt(var_pooled), np.ones((1, n_array))))
 
+    print >>sys.stderr, "Fitting L/S model and finding priors"
     batch_design = design[design.columns[:n_batch]]
     gamma_hat = np.dot(np.dot(la.inv(np.dot(batch_design.T, batch_design)), batch_design.T), s_data.T)
 
@@ -75,7 +82,7 @@ def combat(dat, batch, mod, numCovs=None):
     a_prior = map(aprior, delta_hat)
     b_prior = map(bprior, delta_hat)
 
-    print("finding parametric adjustments")
+    print >>sys.stderr, ("Finding parametric adjustments")
     gamma_star, delta_star = [], []
     for i, batch_idxs in enumerate(batch_info):
         #print '18 20 22 28 29 31 32 33 35 40 46'
@@ -83,7 +90,7 @@ def combat(dat, batch, mod, numCovs=None):
 
         temp = it_sol(s_data[batch_idxs], gamma_hat[i],
                      delta_hat[i], gamma_bar[i], t2[i], a_prior[i], b_prior[i])
-        # NOTE: OK to here!
+
         gamma_star.append(temp[0])
         delta_star.append(temp[1])
 
@@ -97,11 +104,9 @@ def combat(dat, batch, mod, numCovs=None):
 
         dsq = np.sqrt(delta_star[j,:])
         dsq = dsq.reshape((len(dsq), 1))
-        #print dsq
 	denom =  np.dot(dsq, np.ones((1, n_batches[j])))
         numer = np.array(bayesdata[batch_idxs] - np.dot(batch_design.ix[batch_idxs], gamma_star).T)
 
-    	#print batch_design.ix[batch_idxs]
         bayesdata[batch_idxs] = numer / denom
    
     vpsq = np.sqrt(var_pooled).reshape((len(var_pooled), 1))
@@ -123,8 +128,8 @@ def it_sol(sdat, g_hat, d_hat, g_bar, t2, a, b, conv=0.0001):
         d_new = postvar(sum2, n, a, b)
        
         change = max((abs(g_new - g_old) / g_old).max(), (abs(d_new - d_old) / d_old).max())
-        g_old = g_new
-        d_old = d_new
+        g_old = g_new #.copy()
+        d_old = d_new #.copy()
         count = count + 1
     adjust = (g_new, d_new)
     return adjust 
@@ -145,7 +150,6 @@ def postmean(g_hat, g_bar, n, d_star, t2):
     return (t2*n*g_hat+d_star * g_bar) / (t2*n+d_star)
 
 def postvar(sum2, n, a, b):
- 
     return (0.5 * sum2 + b) / (n / 2.0 + a - 1.0)
 
 
